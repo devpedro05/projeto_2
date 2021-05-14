aa06c879d0a3f755343f4d60f52fe9fb94093672
devpedro05 <pedrohenriqueribeiro0405@gmail.com>
2021-05-14 09:21:08 -0300

    mudança no arquivo JS e index

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,26 @@ const contador = document.querySelector('#contador')
 const modificado = document.querySelector('#modificado')
 const efeito = document.querySelector('#efeito')
  contador.innerHTML = 0
+ 
 
-texto.addEventListener('keyup', function(){
- contador.innerHTML = 0 + texto.value.length
-  efeito.options[efeito.selectedIndex].text;
- if(efeito.value == 'uppercase'){
-  modificado.innerHTML= original.value.toUpperCase();
-}
-else{
-modificado.innerHTML= original.value.toLowerCase();
+texto.addEventListener('keyup', function verificar(){
+  contador.innerHTML = 0 + texto.value.length;
+
+   if(efeito.value == 'uppercase'){
+     modificado.innerHTML= original.value.toUpperCase();
+   }
+   else{
+   modificado.innerHTML= original.value.toLowerCase();
+      }
+   
+})
+efeito.addEventListener('change', function(){
+  
+  if(efeito.value == 'uppercase'){
+    modificado.innerHTML= original.value.toUpperCase();
   }
-}) 
+  else{
+  modificado.innerHTML= original.value.toLowerCase();
+     }
 
+})
